c6aa8699f298a840fa853c40050edc975ee66e99
Alibek Zhulmurzin <a.zhulmurzin@gmail.com>
2023-06-11 13:56:09 +0500

    feat: solve 2.10

---
+++ 2.10/2.js
@@ -0,0 +1,6 @@
+const jsName = prompt('What is official name of JavaScript?', '');
+if (jsName === 'ECMAScript') {
+    alert('Correct!');
+} else {
+    alert("Didn't you know? It's ECMAScript!");
+}
---
+++ 2.10/3.js
@@ -0,0 +1,8 @@
+const number = prompt('Enter your number', '');
+if (number > 0) {
+    alert(1);
+} else if (number < 0) {
+    alert(-1);
+} else {
+    alert(0);
+}
